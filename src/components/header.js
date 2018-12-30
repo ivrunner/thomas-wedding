f6583148e533d39dc1984e24553fa05126f76847
ivrunner <richyentam@gmail.com>
2018-12-29 22:01:37 -0600

    made major adjustments to header area.

--- src/components/header.js
+++ src/components/header.js
@@ -5,47 +5,15 @@ import "./style.scss";
 
 import gatsbyLogo from "../images/gatsby-icon.png";
 import bulmaLogo from "../images/bulma-logo.png";
-import Navbar from "./navbar";
+import flower from "../images/flower.jpg";
 
 const Header = ({ siteTitle }) => (
-  <section className="hero gradientBg is-fullheight-with-navbar">
-    <Navbar />
-    <div className="hero-body">
-      <div className="container center">
-        <article className="media">
-          <figure className="is-left">
-            <span className="icon is-large ">
-              <img src={gatsbyLogo} alt="gatsby-logo" />
-            </span>
-          </figure>
-          <figure className="is-left">
-            <span className="icon is-large">
-              <img src={bulmaLogo} alt="bulma-logo" />
-            </span>
-          </figure>
-          <div className="media-content">
-            <div className="content">
-              <h1 className="is-uppercase is-size-1 has-text-white">
-                Thomas Wedding
-              </h1>
-              <p className="subtitle has-text-white is-size-3">
-                A Bulma CSS + GatsbyJS Starter Kit{" "}
-                <a
-                  className="button is-info is-inverted"
-                  href="https://github.com/amandeepmittal/gatsby-bulma-quickstart"
-                >
-                  <span className="icon">
-                    <FaGithub size="fa-2x" />
-                  </span>
-                  <span>Download</span>
-                </a>
-              </p>
-            </div>
-          </div>
-        </article>
+  <section class="hero is-primary is-medium">
+    <div class="hero-body">
+      <div class="container has-text-centered">
+        <h1 class="title">THOMAS WEDDING</h1>
       </div>
     </div>
   </section>
 );
-
 export default Header;
