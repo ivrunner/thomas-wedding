ad6e6175a16d06cb7fe96c2c856a6b4a278c85a4
ivrunner <richyentam@gmail.com>
2018-12-30 10:31:48 -0600

    updated top portion again.

--- src/components/header.js
+++ src/components/header.js
@@ -6,12 +6,13 @@ import "./style.scss";
 import gatsbyLogo from "../images/gatsby-icon.png";
 import bulmaLogo from "../images/bulma-logo.png";
 import flower from "../images/flower.jpg";
+import toplogo from "../images/bk-logo.svg";
 
 const Header = ({ siteTitle }) => (
   <section class="hero is-primary is-medium">
     <div class="hero-body">
       <div class="container has-text-centered">
-        <h1 class="title">THOMAS WEDDING</h1>
+        <img src={toplogo} />
       </div>
     </div>
   </section>
